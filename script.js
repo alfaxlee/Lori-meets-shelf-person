94297dbfa77eb68c28f7ffe1fe548e7f1dec27e6
alfaxlee <alfaxlee@gmail.com>
2026-04-12 15:42:21 +0800

    second commit

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ const config = {
     width: 800,
     height: 600,
     backgroundColor: '#ffffff',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
-    },
     scene: {
         preload: preload,
         create: create
@@ -18,7 +11,10 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+let player; 
+
 function preload() {
+    // 載入指定的圖片
     this.load.image('胖嘟嘟發電機', 'https://yt3.googleusercontent.com/aET0nIXYzBzTkqili3s14Ks_9Vkp6910Ug4ZAP2r_UfkD5dj-Ed-aSqoH52Wv4vbT2MlWtsguQ=s900-c-k-c0x00ffffff-no-rj');
 }
 
@@ -26,5 +22,7 @@ function create() {
     this.player = this.physics.add.group({
         key: '胖嘟嘟發電機'
     });
-    console.log('遊戲已建立，圖片已載入，物理群組已建立');
+    this.player.setScale(0.05)
+    // 如果您想顯示圖片，可以在這裡使用：
+    // this.add.image(400, 300, '胖嘟嘟發電機');
 }
